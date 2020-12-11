feee65c2ae9fe25e274477e476dd2518ae25c1ca
Altareon98 <30687491+Altareon98@users.noreply.github.com>
2020-12-11 07:22:12 -0600

    Ahora si el bueno

---
+++ revisionesLazaro/webService/generarArchivo.py
@@ -0,0 +1,42 @@
+#!C:\Users\DELL\AppData\Local\Programs\Python\Python39\python.exe
+
+import cgi
+import html
+import os
+from conexion import conn
+
+cursor = conn.cursor()
+
+print('Content-type: text/html\n')
+
+form = cgi.FieldStorage()
+
+query = """SELECT * FROM alumno"""
+cursor.execute(query)
+result = cursor.fetchall()
+contenido = "["
+for row in result:
+    ncontrol = str(row[0])
+    nombre = row[1]
+    fnacimiento = str(row[2])
+    telefono = str(row[3])
+    contenido = contenido + '{"Num_Control": "' + ncontrol + '","Nombre": "' + nombre + '","Fecha_Nacimiento": "' + fnacimiento + '","Telefono": "' + telefono + '"},'
+        
+contenido = contenido[:-1]
+contenido = contenido + "]"
+cursor.close()
+conn.close()
+
+print(contenido)
+
+'''
+archivo = open("alumnos.json", 'w')
+archivo.write(contenido)
+archivo.close()
+
+if "alumnos.json" in os.listdir("."):
+    print ("El archivo fue creado!")
+else:
+    print ("El archivo no fue creado!!!")
+'''
+
